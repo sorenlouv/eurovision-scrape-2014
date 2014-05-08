8efb96d5dd38773881193078f378e13b3daf5dfd
Søren Louv-Jansen <slo@tradeshift.com>
2014-05-08 09:32:07 +0200

    added server

--- app.js
+++ app.js
@@ -13,12 +13,10 @@ app.get('/', function(req, res) {
   res.send('');
 });
 
-setInterval(helpers.getVideos, 1000000);
-console.log('Started...');
+var server = app.listen(3000, function() {
+  // Run every hour
+  setInterval(helpers.getVideos, 1000000);
+  console.log('Started...');
 
-// var server = app.listen(3000, function() {
-//   // Run every hour
-
-
-//   console.log('Listening on port %d', server.address().port);
-// });
+  console.log('Listening on port %d', server.address().port);
+});
