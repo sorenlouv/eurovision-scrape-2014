f0261ecaa9af9bba125a3d2e5c1de1f1ed04c8d4
Søren Louv-Jansen <slo@tradeshift.com>
2014-05-08 10:03:21 +0200

    getTimeLeft

--- app.js
+++ app.js
@@ -6,20 +6,26 @@ var app = express();
 var helpers = require('./helpers');
 var mongoose = require('mongoose');
 var Video = require('./video');
+var getVideosInterval;
 
 mongoose.connect('mongodb://localhost/eurovision');
 
 app.get('/', function(req, res) {
   Video.count(function(err, count){
     res.statusCode = 200;
-    res.send('count: ' + count);
+    res.send('count: ' + count + '\n Time to next: ' + getTimeLeft(getVideosInterval));
   });
 });
 
+var getTimeLeft = function(interval){
+  return Math.ceil((interval._idleStart + interval._idleTimeout - Date.now()) / 1000);
+};
+
 var server = app.listen(3000, function() {
   // Run every hour
-  setInterval(helpers.getVideos, 1000000);
-  console.log('Started...');
+  var intervalDuration = 1000000;
+  getVideosInterval = setInterval(helpers.getVideos, intervalDuration);
+  console.log('Scraping every ' + (intervalDuration/(1000*60)) + ' minute');
 
   console.log('Listening on port %d', server.address().port);
 });
